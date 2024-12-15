6e1249dd402c41903bf4006a7bb6c9bed6e94194
Lisa <lizamotorna2004@gmail.com>
2024-12-16 00:57:08 +0200

    add plants leaves and animation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,36 @@
 import "../styles/style.scss";
+
+const firstPlant = document.querySelectorAll(".intro__images img")[0];
+const secondPlant = document.querySelectorAll(".intro__images img")[1];
+const thirdPlant = document.querySelectorAll(".intro__images img")[2];
+
+const plants = secondPlant.parentElement.parentElement;
+
+plants.onmouseover = () => {
+  secondPlant.parentElement.classList.remove(
+    "intro__descr-notHover"
+  );
+  secondPlant.parentElement.classList.add(
+    "intro__descr-hover"
+  );
+};
+
+plants.onmouseleave = () => {
+  secondPlant.parentElement.classList.remove(
+    "intro__descr-hover"
+  );
+  secondPlant.parentElement.classList.add(
+    "intro__descr-notHover"
+  );
+};
+
+const handleMouseLeave = () => {
+  secondPlant.style.top = "50%";
+};
+
+[firstPlant, thirdPlant].forEach((plant) => {
+  plant.onmouseover = () => {
+    secondPlant.style.top = `calc(50% + ${plant === firstPlant ? 25 : -25}px)`;
+  };
+  plant.onmouseleave = handleMouseLeave;
+});
